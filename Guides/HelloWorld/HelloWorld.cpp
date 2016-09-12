8a15b745da88bd70c5f083e9c3a1ac43c4d3e2bc
Danny Ng <danny@netizentesting.com>
2016-09-12 10:48:37 +0800

    Initial Commit

---
+++ Guides/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"				// Precompiled header for Win32 Application
+#include <iostream>				// Header for standard input and output stream objects
+
+using namespace std;			// Standard namespace which contains for object in iostream 
+
+// 
+int main()
+{
+	cout << "Hello World\n";
+    return 0;
+}
