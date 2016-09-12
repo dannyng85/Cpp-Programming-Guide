a9181f9622b4a48eebeb71900e23025fc1781c96
Danny Ng <danny@netizentesting.com>
2016-09-12 11:42:15 +0800

    Updated Comments

--- Guides/HelloWorld/HelloWorld.cpp
+++ Guides/HelloWorld/HelloWorld.cpp
@@ -7,5 +7,5 @@ using namespace std;			// Standard namespace which contains for object in iostre
 int main()
 {
 	cout << "Hello World\n";	// String to display by the program
-    return 0;					// Return 0 let the OS know that there is no error from the program execution
+    return 0;					// Return 0 let the operating system know that there is no error from the program execution
 }
