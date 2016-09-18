a0b0b7023a7ec16271373a15875f7a8941ada822
Danny Ng <danny@netizentesting.com>
2016-09-18 21:05:33 +0800

    Added cin and cout

---
+++ Guides/cout/cout.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// declaration of the variable number with type of int
+	int number = 1;
+	// declaration of the variable number2 with type of double
+	double number2 = 10.023;
+	cout << "This is";
+	cout << " in a line.\n";
+	cout << number << " is one in word\n";
+	cout << number2 << "\n";
+	return 0;
+}
+
